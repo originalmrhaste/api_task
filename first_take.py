444ff77930c8722410f6fcfea0e0c4773cabe622
originalmrhaste <originalmrhaste@gmail.com>
2025-02-23 10:55:40 +0100

    Adding pagination, adding simplest rate limit, changing config from argparse to dict

--- first_take.py
+++ first_take.py
@@ -1,10 +1,12 @@
 import argparse
-from dotenv import load_dotenv
-from pprint import pprint
 import logging
-import requests
 import os
-import json
+import requests
+import time
+
+from dotenv import load_dotenv
+from helpers import flatten_dict
+from pprint import pprint
 
 """
 Skeleton for Squirro Delivery Hiring Coding Challenge
@@ -12,6 +14,7 @@ January 2024
 """
 log = logging.getLogger(__name__)
 BASE_URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+MAX_ARTICLES_PER_REQUEST = 10
 
 
 class NYTimesSource:
@@ -19,8 +22,15 @@ class NYTimesSource:
     A data loader plugin for the NY Times API.
     """
 
-    def __init__(self):
-        self.args = {}
+    def __init__(self, config):
+        # I changed this to use the config dict instead of args,
+        # since args are arbitrarily called by the caller and class
+        # then doesn't have control over the config
+        self.payload = {
+            "q": config.get("query", None),
+            "api-key": config.get("api_key", None),
+            "page": config.get("page", 0),
+        }
 
     def connect(self, inc_column=None, max_inc_value=None):
         """Connect to the source"""
@@ -32,27 +42,51 @@ class NYTimesSource:
         # Nothing to do
         pass
 
-    def getDataBatch(self, batch_size=0):
+    def getDataBatch(self, batch_size=10):
         """
         Generator - Get data from source on batches.
         :returns One list for each batch. Each of those is a
         list of
         dictionaries with the defined rows.
         """
-        # TODO: implement - this dummy implementation returns one batch of data
 
-        payload = {
-            "q": getattr(self.args, "query", None),
-            "api-key": getattr(self.args, "api_key", None),
-        }
-        response = requests.get(url=BASE_URL, params=payload)
-        return response.json()
+        articles = []
+        for article in self.getArticles(batch_size):
+            articles.append(flatten_dict(article))
+            if len(articles) >= batch_size:
+                yield articles
+                articles = []
+
+        # can omit this
+        if articles:
+            yield articles
         # yield [
         #     {
         #         "headline.main": "The main headline",
         #         "_id": "1234",
         #     }
         # ]
+
+    def getArticles(self, batch_size):
+        total_articles = batch_size
+        while total_articles > 0:
+            response = requests.get(url=BASE_URL, params=self.payload)
+            match response.status_code:
+                case 200:
+                    for article in response.json()["response"]["docs"]:
+                        yield article
+                        total_articles -= 1
+                case 401:
+                    raise Exception("Unauthorized request.  Make sure api-key is set.")
+                case 429:
+                    raise Exception(
+                        "Too many requests.  You reached your per minute or per day rate limit."
+                    )
+                case _:
+                    raise Exception(f"Unknown error: {response.status_code}")
+            self.payload["page"] += 1
+            # one way to always be sure to not hit the rate limit
+            time.sleep(12)
 
     def getSchema(self):
         """
@@ -75,17 +109,17 @@ class NYTimesSource:
 
 if __name__ == "__main__":
     load_dotenv()
-    config = {"api_key": os.getenv("NYTIMES_API_KEY"), "query": "Silicon Valley", "filter": "title"}
+    config = {
+        "api_key": os.getenv("NYTIMES_API_KEY"),
+        "query": "Silicon Valley",
+        "page": 20,
+    }
     pprint(config)
-    source = NYTimesSource()
+    source = NYTimesSource(config)
     # This looks like an argparse dependency - but theNamespace class is just
     # a simple way to create an object holding attributes.
-    source.args = argparse.Namespace(**config)
-    response = source.getDataBatch()
-    pprint(response)
-    with open("response.json", "w") as f:
-        json.dump(response, f)
-    # for idx, batch in enumerate(source.getDataBatch(10)):
-    #     print(f"{idx} Batch of {len(batch)} items")
-    #     for item in batch:
-    #         print(f" - {item['_id']} - {item['headline.main']}")
+    # source.args = argparse.Namespace(**config)
+    for idx, batch in enumerate(source.getDataBatch(15)):
+        print(f"{idx} Batch of {len(batch)} items")
+        for item in batch:
+            print(f" - {item['_id']} - {item['headline.main']}")
--- nytimes_test.py
+++ nytimes_test.py
@@ -3,15 +3,23 @@ import argparse
 
 
 def test_correct_url_and_params(mocker):
-    mock_get = mocker.patch("requests.get")
+    mocker.patch("time.sleep")
+    mock_response = mocker.Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = {
+        "response": {
+            "docs": [{"headline": {"main": f"test_article_{i}"}} for i in range(10)]
+        }
+    }
+    mock_get = mocker.patch("requests.get", return_value=mock_response)
 
-    test_config = {"query": "test_query", "api_key": "test_api_key"}
-    nytimes = NYTimesSource()
-    nytimes.args = argparse.Namespace(**test_config)
-    nytimes.getDataBatch()
+    test_config = {"query": "test_query", "api_key": "test_api_key", "page": 10}
+    nytimes = NYTimesSource(test_config)
+    list(nytimes.getDataBatch())
 
     mock_get.assert_called_once()
 
-    result_params = {"q": "test_query", "api-key": "test_api_key"}
+    # its 11 because the payload passed to mock updates in place
+    result_params = {"q": "test_query", "api-key": "test_api_key", "page": 11}
     result_url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
     mock_get.assert_called_with(url=result_url, params=result_params)
